ea8cafc74d0a2818562d817ea5eff0fb0054ceea
Aldenir dos Santos <89494499+AldenirTakeshi@users.noreply.github.com>
2026-04-29 16:15:00 -0400

    feat: adiciona novas endpoints de math e move logica para service.

--- spring-boot-rest/src/main/java/br/com/takeshi/spring_boot_rest/controllers/MathController.java
+++ spring-boot-rest/src/main/java/br/com/takeshi/spring_boot_rest/controllers/MathController.java
@@ -1,29 +1,51 @@
 package br.com.takeshi.spring_boot_rest.controllers;
 
 import br.com.takeshi.spring_boot_rest.exception.UnsupportedMathOperationException;
+import br.com.takeshi.spring_boot_rest.service.MathService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
 
+    private final MathService mathService;
+
+    public MathController(MathService mathService) {
+        this.mathService = mathService;
+    }
+
     @RequestMapping("/sum/{number1}/{number2}")
     public Double sum(@PathVariable("number1") String number1, @PathVariable("number2") String number2) throws Exception {
-        if(!isNumeric(number1) || !isNumeric(number2)) throw new UnsupportedMathOperationException("Please set a numeric value!");
-        return convertToDouble(number1) + convertToDouble(number2);
+        return mathService.sum(number1,number2);
     }
 
-    public Boolean isNumeric(String strNumber){
-        if(strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replace(",","."); // R$ 5,00
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    @RequestMapping("/subtraction/{number1}/{number2}")
+    public Double subtraction(@PathVariable("number1") String number1, @PathVariable("number2") String number2) throws Exception {
+        return mathService.subtraction(number1, number2);
     }
 
-    public Double convertToDouble(String strNumber) {
-        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value!");
-        String number = strNumber.replace(",","."); // R$ 5,00
-        return Double.parseDouble(number);
+    @RequestMapping("/multiplication/{number1}/{number2}")
+    public Double multiplication(@PathVariable("number1") String number1, @PathVariable("number2") String number2) throws Exception {
+        return mathService.multiplication(number1, number2);
+    }
+
+    @RequestMapping("/division/{number1}/{number2}")
+    public Double division(@PathVariable("number1") String number1, @PathVariable("number2") String number2) throws Exception {
+        return mathService.division(number1, number2);
+    }
+
+    @RequestMapping("/mean/{number1}/{number2}")
+    public Double mean(@PathVariable("number1") String number1, @PathVariable("number2") String number2) throws Exception {
+        return mathService.mean(number1, number2);
+    }
+
+    @RequestMapping("/squareroot/{number1}/{number2}")
+    public List<Double> squareRoot(@PathVariable("number1") String number1, @PathVariable("number2") String number2) throws Exception {
+        return mathService.squareRoot(number1, number2);
     }
 }
---
+++ spring-boot-rest/src/main/java/br/com/takeshi/spring_boot_rest/service/MathService.java
@@ -0,0 +1,66 @@
+package br.com.takeshi.spring_boot_rest.service;
+
+import br.com.takeshi.spring_boot_rest.exception.UnsupportedMathOperationException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MathService {
+
+    public Double sum(String number1, String number2){
+        if(!isNumeric(number1) || !isNumeric(number2)) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        return convertToDouble(number1) + convertToDouble(number2);
+    }
+
+    public  Double subtraction(String number1, String number2){
+        if(!isNumeric(number1) || !isNumeric(number2)) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        return convertToDouble(number1) - convertToDouble(number2);
+    }
+
+    public Double multiplication(String number1, String number2){
+        if(!isNumeric(number1) || !isNumeric(number2)) throw new UnsupportedMathOperationException("Please  set a number value");
+        return convertToDouble(number1) * convertToDouble(number2);
+    }
+
+    public Double division(String number1, String number2){
+        if(!isNumeric(number1) || !isNumeric(number2)) throw new UnsupportedMathOperationException("Please  set a number value");
+        return convertToDouble(number1) / convertToDouble(number2);    }
+
+    public Double mean(String number1, String number2){
+        if(!isNumeric(number1) || !isNumeric(number2)) throw new UnsupportedMathOperationException("Please  set a number value");
+        return convertToDouble(number1) * convertToDouble(number2) / 2;    }
+
+    public List<Double> squareRoot(String number1, String number2){
+        if(!isNumeric(number1) || !isNumeric(number2)) throw new UnsupportedMathOperationException("Please  set a number value");
+        double n1 = convertToDouble(number1);
+        double n2 = convertToDouble(number2);
+
+        double min = Math.min(n1,n2);
+        double max = Math.max(n1,n2);
+
+        List<Double> squares = new ArrayList<>();
+
+        int start = (int) Math.ceil(Math.sqrt(min));
+        int end = (int) Math.floor(Math.sqrt(max));
+
+        for (int i = start; i <= end ; i++) {
+            squares.add((double) i * i);
+        }
+
+        return squares;
+    }
+
+    public static Boolean isNumeric(String strNumber){
+        if(strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",","."); // R$ 5,00
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    public static Double convertToDouble(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value!");
+        String number = strNumber.replace(",","."); // R$ 5,00
+        return Double.parseDouble(number);
+    }
+}
